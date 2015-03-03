18fb2b4dfbdb6122fb0513d508ed0788473b1ae0
miso <misodengaku@gmail.com>
2015-03-04 01:04:41 +0900

    rotate

--- gv.rb
+++ gv.rb
@@ -8,6 +8,7 @@ class Gv
     file_name = File::basename(file_path)
     file_dir = File::dirname(file_path)
     @isFullscreen = false
+    @rotate = 0
 
 
     # FilePath
@@ -35,7 +36,7 @@ class Gv
       Gtk.main_quit
     end
 
-    show_image(@files[@index])
+    show_image(@files[@index], true)
 
 
     @window.add(@image)
@@ -44,45 +45,111 @@ class Gv
 
   end
 
-  def show_image(file_name)
+  def show_image(file_name, isFirst = nil)
     p file_name
     @pixbuf = Gdk::Pixbuf.new(file_name)
-    puts "#{@screen.width}x#{@screen.height}"
-    if @pixbuf.width > @pixbuf.height
-      if @screen.width < @pixbuf.width
-        puts 'resize require: width'
-        scale = @screen.width.to_f / @pixbuf.width
-        puts "#{(@pixbuf.width * scale).to_i}x#{(@pixbuf.height * scale).to_i}"
-        @pixbuf = @pixbuf.scale((@pixbuf.width * scale).to_i, (@pixbuf.height * scale).to_i, Gdk::Pixbuf::INTERP_HYPER)
-        @image.set(@pixbuf)
-      end
-    else
-      if @screen.height < @pixbuf.height
-        puts 'resize require: height'
-        scale = @screen.height.to_f / @pixbuf.height
-        puts "#{(@pixbuf.width * scale).to_i}x#{(@pixbuf.height * scale).to_i}"
-        @pixbuf = @pixbuf.scale((@pixbuf.width * scale).to_i, (@pixbuf.height * scale).to_i, Gdk::Pixbuf::INTERP_HYPER)
-        @image.set(@pixbuf)
-      end
-    end
-
-
+    @org_pix = @pixbuf.dup
+    pixbuf_draw(isFirst)
     @window.title = "#{file_name} - #{@index + 1} / #{@files.count}"
+  end
+
+  def pixbuf_rotate(angle)
+    puts "angle: #{angle}"
+    @pixbuf = @org_pix.dup
+    for i in 1..angle do
+      @pixbuf = @pixbuf.rotate(Gdk::Pixbuf::ROTATE_CLOCKWISE)
+    end
+    pixbuf_draw()
+  end
+
+  def pixbuf_draw(size = nil)
+    puts "Screen: #{@screen.width}x#{@screen.height}"
+    if size.nil? & !@isFullscreen
+      if @pixbuf.width > @pixbuf.height
+        if @window.size[0] < @pixbuf.width
+          puts 'resize require: width'
+          scale = @window.size[0].to_f / @pixbuf.width
+          puts "#{(@pixbuf.width * scale).to_i}x#{(@pixbuf.height * scale).to_i}"
+          @pixbuf = @pixbuf.scale((@pixbuf.width * scale).to_i, (@pixbuf.height * scale).to_i, Gdk::Pixbuf::INTERP_HYPER)
+        end
+      else
+        if @window.size[1] < @pixbuf.height
+          puts 'resize require: height'
+          scale = @window.size[1].to_f / @pixbuf.height
+          puts "#{(@pixbuf.width * scale).to_i}x#{(@pixbuf.height * scale).to_i}"
+          @pixbuf = @pixbuf.scale((@pixbuf.width * scale).to_i, (@pixbuf.height * scale).to_i, Gdk::Pixbuf::INTERP_HYPER)
+        end
+      end
+      #@window.set_size_request(@pixbuf.width, @pixbuf.height)
+    else
+      if @pixbuf.width > @pixbuf.height
+        if @screen.width < @pixbuf.width
+          puts 'resize require: width mode screen'
+          scale = @screen.width.to_f / @pixbuf.width
+          puts "#{(@pixbuf.width * scale).to_i}x#{(@pixbuf.height * scale).to_i}"
+          @pixbuf = @pixbuf.scale((@pixbuf.width * scale).to_i, (@pixbuf.height * scale).to_i, Gdk::Pixbuf::INTERP_HYPER)
+        end
+      else
+        if @screen.height < @pixbuf.height
+          puts 'resize require: height mode screen'
+          scale = @screen.height.to_f / @pixbuf.height
+          puts "#{(@pixbuf.width * scale).to_i}x#{(@pixbuf.height * scale).to_i}"
+          @pixbuf = @pixbuf.scale((@pixbuf.width * scale).to_i, (@pixbuf.height * scale).to_i, Gdk::Pixbuf::INTERP_HYPER)
+        end
+      end
+
+    end
+    @image.set(@pixbuf)
   end
 
   def key_event(e)
     key = Gdk::Keyval.to_name(e.keyval)
+    p key
     case key
       when 'Right'
-        if @files.count > @index + 1
-          @index = @index + 1
+        if Gdk::Window::ModifierType::CONTROL_MASK == e.state & Gdk::Window::CONTROL_MASK
+          if @files.count > @index + 10
+            @index = @index + 10
+          else
+            @index = @files.count - 1
+          end
           show_image(@files[@index])
+        else
+          if @files.count > @index + 1
+            @index = @index + 1
+            show_image(@files[@index])
+          end
         end
       when 'Left'
-        if @index - 1 >= 0
-          @index = @index - 1
+        if Gdk::Window::ModifierType::CONTROL_MASK == e.state & Gdk::Window::CONTROL_MASK
+          if @index - 10 >= 0
+            @index = @index - 10
+          else
+            @index = 0
+          end
           show_image(@files[@index])
+        else
+          if @index - 1 >= 0
+            @index = @index - 1
+            show_image(@files[@index])
+          end
         end
+      when 'period'
+        puts 'rotate: CClock'
+        @rotate = @rotate + 1
+        if @rotate > 3
+          @rotate = 0
+        end
+        pixbuf_rotate(@rotate)
+      when 'comma'
+        puts 'rotate: Clock'
+        @rotate = @rotate - 1
+        if @rotate < 0
+          @rotate = 3
+        end
+        pixbuf_rotate(@rotate)
+
+
       when 'Return'
         puts 'enter'
         if !@isFullscreen
@@ -92,6 +159,8 @@ class Gv
           @window.unfullscreen
           @isFullscreen = false
         end
+      when 'Escape'
+        exit(0)
     end
 
   end
